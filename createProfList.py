259a70d4ea764d45bc7e2fb7c69431531dbf0a07
jipson7 <caleb.phillips@uoit.net>
2015-03-10 16:09:09 -0400

    createProfList pulls the uoit directory and puts it in profList.txt, still need to have it extract name

--- createProfList.py
+++ createProfList.py
@@ -1,3 +1,16 @@
 import os
+import urllib2
+import re
+
+URL_BASE = "https://ssbp.mycampus.ca/prod/www_directory.directory_uoit.p_ShowDepartment?department_name_in="
+
+URL_LIST = ["U1", "U2", "7519", "U3", "U4", "U7", "14111"]
 
 
+for item in URL_LIST:
+
+	URL = URL_BASE + URL_LIST
+
+	bashCommand = ('wget -0 - ' + URL + ' >> profList.txt')
+
+	os.system(bashCommand)

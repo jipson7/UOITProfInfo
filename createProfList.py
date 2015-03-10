642aea6e6831effc90dc29f232b3e9dcd6ca0739
jipson7 <caleb.phillips@uoit.net>
2015-03-10 16:28:05 -0400

    CreateProfList successfully creates a txt file containing a list of uoit faculty

--- createProfList.py
+++ createProfList.py
@@ -6,11 +6,33 @@ URL_BASE = "https://ssbp.mycampus.ca/prod/www_directory.directory_uoit.p_ShowDep
 
 URL_LIST = ["U1", "U2", "7519", "U3", "U4", "U7", "14111"]
 
+profList = []
+
+def parseNames(page):
+
+	global profList
+
+	nameRegex =	r'<TD><a href="www_directory.directory_uoit.p_showindividual\?individual_id_in=[0-9]+&home_url_in=">([A-Za-z,.&\- ]+)<\/a>'
+
+	namePattern = re.compile(nameRegex)
+
+	for (name) in re.findall(namePattern, page):
+
+		profList.append(name)
+
 
 for item in URL_LIST:
 
-	URL = URL_BASE + URL_LIST
 
-	bashCommand = ('wget -0 - ' + URL + ' >> profList.txt')
 
-	os.system(bashCommand)
+	currentPage = urllib2.urlopen(URL_BASE + item).read()
+
+	parseNames(currentPage)
+
+f = open("profList.txt", "w")
+
+for name in profList:
+
+	f.write(name + "\n")
+
+

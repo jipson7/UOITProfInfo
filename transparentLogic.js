2bd24ab04eb1e1716954dc898314ecf7e7fc8708
jipson7 <caleb.phillips@uoit.net>
2015-03-10 17:23:15 -0400

    Added some jquery tests, too see if I can find all the names in a page and put a button beside them, so far so good

--- transparentLogic.js
+++ transparentLogic.js
@@ -1,10 +1,33 @@
 
 $( document ).ready(function(){
+
+	//THIS IS A NAME CHANGING TEST
+	//it searches for any element without children, sees if it contains a string, and changes it
+	//
 	
+
+	//Find everything without children
+	var childList = $(":not(:has(*))");
+	
+	//find everything that contains 'find me'
+	var nameTags = childList.filter(":contains('find me')");
+
+	//append a button to each of them
+	nameTags.each(function() {
+	
+		$(this).append("<button>TEST</button>");
+	
+	});
+
+	
+});
+
+function testPHP() {
+
 	$.get("ratings.php", function(data){
 			
 		$("#output").text(data);
 	
 	});
 
-});
+}

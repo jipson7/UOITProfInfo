53bd2903db7db6c172a242a7bc72ee7572c89935
jipson7 <caleb.phillips@uoit.net>
2015-03-10 18:08:44 -0400

    Update js file to scan dom for prof names. If it finds one it will inject a button beside it

--- transparentLogic.js
+++ transparentLogic.js
@@ -1,25 +1,34 @@
+//This function is suppose to make jQuery contains selector case incensitive
+//// found here https://css-tricks.com/snippets/jquery/make-jquery-contains-case-insensitive/
+$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
 
 $( document ).ready(function(){
 
-	//THIS IS A NAME CHANGING TEST
-	//it searches for any element without children, sees if it contains a string, and changes it
-	//
-	
-
-	//Find everything without children
 	var childList = $(":not(:has(*))");
 	
-	//find everything that contains 'find me'
-	var nameTags = childList.filter(":contains('find me')");
+	var nameTags;
 
-	//append a button to each of them
-	nameTags.each(function() {
-	
-		$(this).append("<button>TEST</button>");
-	
-	});
+	for(var i = 0; i < PROF_MASTERLIST.length; i++){
 
-	
+		nameTags = childList.filter(":contains('" + PROF_MASTERLIST[i] + "')");
+
+		if (nameTags.length !== 0) {
+
+			nameTags.each(function() {
+			
+				$(this).append("<button>TEST</button>");	
+			
+			});	
+
+		}
+
+	}
+		
 });
 
 function testPHP() {
